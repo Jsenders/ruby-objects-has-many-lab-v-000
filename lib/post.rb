eaf776d21ada3c2ad2de5f834b88568f5468237e
Joseph Senders <jasenders@gmail.com>
2018-08-21 20:12:56 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,17 @@
 class Post
   attr_accessor :author, :title
 
+@@all = []
+
   def initialize(title)
     self.title = title
+    @@all << self
   end
+
+def self.all
+  return
+  @@all = []
+end
 
   def author_name
     self.author ? self.author.name : nil
